c0139e960f611dab862e7b78040e5131c575047c
xihe <xihe@2dfire.com>
2018-09-19 22:21:04 +0800

    finish seed play

---
+++ Seed.py
@@ -0,0 +1,67 @@
+"""
+ * @Author: xihe 
+ * @Date: 2018-09-19 22:14:27 
+ * @Last Modified by:   xihe 
+ * @Last Modified time: 2018-09-19 22:14:27 
+"""
+
+from urllib.request import urlopen
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import re
+import datetime
+import random
+
+pages = set()
+random.seed(datetime.datetime.now())
+
+# 获取页面所有内链的列表
+def getInternalLinks(bsObj, includeUrl):
+    includeUrl = urlparse(includeUrl).scheme+"//"+urlparse(includeUrl).netloc
+    internalLinks = []
+    # 找出所有以 / 开头的链接
+    for link in bsObj.findAll("a", href=re.compile("^(/|.*"+includeUrl+")")):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in internalLinks:
+                if (link.attrs['href'].startswith('/')):
+                    internalLinks.append(includeUrl+link.attrs['href'])
+                else:
+                    internalLinks.append(link.attrs['href'])
+    return internalLinks
+
+# 获取页面所有外链的列表
+def getExternalLinks(bsObj, excludeUrl):
+    externalLinks = []
+    # 找出所有以 http 或者 www 来头并且不包含当前 url 的链接
+    for link in bsObj.findAll("a", href = re.compile("^(http|www)((?!"+excludeUrl+").)*$")):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in externalLinks:
+                externalLinks.append(link.attrs['href'])
+    return externalLinks
+
+def splitAddress(address):
+    addressParts = address.replace("http://","").split("/")
+    return addressParts
+
+def getRandomExternalLink(startingPage):
+    print("---->",startingPage)
+    html = urlopen(startingPage)
+    bsObj = BeautifulSoup(html,'lxml')
+    externalLinks = getExternalLinks(bsObj, urlparse(startingPage).netloc)
+    if len(externalLinks) == 0:
+        print("No external links, looking arround the site for one")
+        domain = urlparse(startingPage).scheme+"://"+urlparse(startingPage).netloc
+        internalLinks = getInternalLinks(bsObj, domain)
+        return getRandomExternalLink(internalLinks[random.randint(0, len(internalLinks) -1 )])
+
+    else:
+        return externalLinks[random.randint(0,len(externalLinks) - 1)]
+
+
+def followExternalOnly(startingSite):
+    externalLink = getRandomExternalLink(startingSite)
+    print("Random external link is: "+externalLink)
+    followExternalOnly(externalLink)
+
+if __name__ == '__main__':
+    followExternalOnly("http://oreilly.com")
